7abf203816c08dabda8f03e9be7a1df6799bcb9e
soyyyyy <da5868@naver.com>
2025-02-16 02:44:53 +0900

    feat.회원가입 백 연동

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import AllList from './pages/AllList.jsx';
 import Complete from './pages/Complete.jsx';
 import './App.css';
 import Router from './Router';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
   return (
     <>
         <div className="App">
+        <ToastContainer />
           <div className='main'>
                 <Router />
           </div>
